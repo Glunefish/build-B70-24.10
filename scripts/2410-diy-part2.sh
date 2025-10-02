f6e09cdc4d537d3af4e1f15ddd78bef52bd6b4b6
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-02 19:46:35 +0800

    Create 2410-diy-part2.sh

---
+++ scripts/2410-diy-part2.sh
@@ -0,0 +1,7 @@
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
+git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/luci-app-adguardhome
+git clone https://github.com/zzsj0928/luci-app-pushbot ./package/luci-app-pushbot
+git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
