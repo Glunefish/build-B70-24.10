6a3cd096729760f94fa1a749284a26805e43818e
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-02 19:48:21 +0800

    Update 2410-diy-part3.sh

--- scripts/2410-diy-part3.sh
+++ scripts/2410-diy-part3.sh
@@ -12,6 +12,7 @@
 
 # Modify login IP
 # sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
 
 # Modify default SSID
 # sed -i "s/ImmortalWrt-2.4G/Breakwa11/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -21,4 +22,4 @@
 # sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
 
 # Insert two lines before the last line in 99-default-settings-chinese. For Modify opkg url, change mt7981 to filogic, del lines inlude passwall.
-sed -i '/^exit 0$/i sed -i "s,mt7981,filogic,g" "package/base-files/files/etc/opkg/distfeeds.conf"\nsed -i '\''/passwall/d'\'' "package/base-files/files/etc/opkg/distfeeds.conf"' package/emortal/default-settings/files/99-default-settings-chinese
+#sed -i '/^exit 0$/i sed -i "s,mt7981,filogic,g" "package/base-files/files/etc/opkg/distfeeds.conf"\nsed -i '\''/passwall/d'\'' "package/base-files/files/etc/opkg/distfeeds.conf"' package/emortal/default-settings/files/99-default-settings-chinese
